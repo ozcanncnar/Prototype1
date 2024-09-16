e5d0b6cc346f96884f185778ed29bc4e375e367e
ozcanncnar <163649061+ozcanncnar@users.noreply.github.com>
2024-09-16 15:11:10 +0300

    kutular tamam

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -14,5 +14,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        
     }
 }

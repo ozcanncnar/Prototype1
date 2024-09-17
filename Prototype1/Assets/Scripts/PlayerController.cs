b81fb4eee46feb21dc62bb9df29da54f7233ca97
ozcanncnar <163649061+ozcanncnar@users.noreply.github.com>
2024-09-17 19:54:01 +0300

    Input Eklenecek

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float turnSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime *  speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
         
     }
 }

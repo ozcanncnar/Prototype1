ec1501e82c03308d9ae0d1752dfcc3813f58e4fb
ozcanncnar <163649061+ozcanncnar@users.noreply.github.com>
2024-09-24 23:18:37 +0300

    local muti yapıldı

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera; 
+    public Camera hoodCamera; 
+    public KeyCode switchKey;
+    public string inputID;
     //private variables
     private float speed = 20.0f;
     private float turnSpeed = 45.0f;
@@ -18,12 +22,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal"+inputID);
+        verticalInput = Input.GetAxis("Vertical"+inputID);
         //Move the car forward based on verticalinput
         transform.Translate(Vector3.forward * Time.deltaTime *  speed * verticalInput);
         //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+
+        if (Input.GetKeyDown(switchKey)) 
+        { mainCamera.enabled = !mainCamera.enabled;
+          hoodCamera.enabled = !hoodCamera.enabled; 
         
+        }
+
     }
 }
